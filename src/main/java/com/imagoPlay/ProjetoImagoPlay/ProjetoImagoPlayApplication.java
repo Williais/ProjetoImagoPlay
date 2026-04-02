6dd210a2cb11c670c4b2139061133d2ae4c46164
Willian Padilha <45665895+Williais@users.noreply.github.com>
2026-04-01 21:26:22 -0300

    Initial commit

---
+++ src/main/java/com/imagoPlay/ProjetoImagoPlay/ProjetoImagoPlayApplication.java
@@ -0,0 +1,13 @@
+package com.imagoPlay.ProjetoImagoPlay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoImagoPlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoImagoPlayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imagoPlay/ProjetoImagoPlay/ProjetoImagoPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imagoPlay.ProjetoImagoPlay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoImagoPlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

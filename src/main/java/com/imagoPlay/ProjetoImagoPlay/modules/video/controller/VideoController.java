292a829709e0d92eb774eeee8899ea1114042c1f
Willian Padilha <45665895+Williais@users.noreply.github.com>
2026-05-05 00:03:16 -0300

    iniciando o Video Model

--- src/main/java/com/imagoPlay/ProjetoImagoPlay/modules/video/controller/VideoController.java
+++ src/main/java/com/imagoPlay/ProjetoImagoPlay/modules/video/controller/VideoController.java
@@ -1,5 +1,6 @@
 package com.imagoPlay.ProjetoImagoPlay.modules.video.controller;
 
+import com.imagoPlay.ProjetoImagoPlay.modules.video.service.VideoService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,4 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/video")
 public class VideoController {
+    private final VideoService videoService;
+
+    public VideoController(VideoService service){
+        this.videoService = service;
+    }
 }
